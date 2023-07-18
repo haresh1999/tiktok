b11c798729db4c854ab1614ea0ec9ff185b76bf0
Haresh Chauhan <hareshc1999@gmail.com>
2023-07-18 16:50:05 +0530

    udpate

--- resources/views/mail/forget-password-mail.blade.php
+++ resources/views/mail/forget-password-mail.blade.php
@@ -3,7 +3,7 @@
 <x-mail::button :color="'red'" :url="'https://tiktok-swipe--poc.web.app/reset?token='.$tok">
 Forget Password
 </x-mail::button>
-<p>Please Note : Token will expire in next 5 min!</p>
+<p>Note : Link will expire in next 5 min!</p>
 Thanks,<br>
 {{ config('app.name') }}
 </x-mail::message>
